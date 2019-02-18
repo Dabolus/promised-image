8d498ab9e8788bebefa5f75a5fb6c85579976d34
Giorgio Garasto <giorgio@garasto.it>
2019-02-18 08:06:09 +0100

    feat: Add a PromisedImage constructor
    PromisedImage works pretty much like the native Image constructor, but instead of constructing an HTMLImageElement, it constructs an HTMLPromisedImageElement.

--- src/promised-image.ts
+++ src/promised-image.ts
@@ -13,3 +13,21 @@ export class HTMLPromisedImageElement extends HTMLImageElement {
 }
 
 customElements.define('promised-image', HTMLPromisedImageElement, { extends: 'img' });
+
+export function PromisedImage(this: HTMLPromisedImageElement, width: number | string, height: number | string) {
+  if (!(this instanceof HTMLPromisedImageElement)) {
+    // tslint:disable-next-line:max-line-length
+    throw new TypeError(`Failed to construct 'PromisedImage': Please use the 'new' operator, this DOM object constructor cannot be called as a function.`);
+  }
+  const image = document.createElement('img', {
+    is: 'promised-image',
+  });
+  image.setAttribute('is', 'promised-image');
+  if (width) {
+    image.setAttribute('width', `${width}`);
+  }
+  if (height) {
+    image.setAttribute('height', `${height}`);
+  }
+  return image;
+}
